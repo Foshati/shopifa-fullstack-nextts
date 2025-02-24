03ecbf6df0d6ee99477c9e20f84d78dedd82bc0d
Foshati <amirrezafoshati@gmail.com>
2025-02-24 22:27:52 +0330

    fix eslint 15

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,76 +1,24 @@
-import { dirname as pathDirname } from 'path';
+import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
-import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 
-const filename = fileURLToPath(import.meta.url);
-const currentDirname = pathDirname(filename);
-const compat = new FlatCompat({ baseDirectory: currentDirname });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const eslintConfig = [
-  ...compat.extends(
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
-  ),
-  {
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-      react: reactPlugin,
-      'react-hooks': reactHooksPlugin,
-      'jsx-a11y': jsxA11yPlugin,
-    },
-    languageOptions: {
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: './tsconfig.json',
-      },
-    },
-    rules: {
-      // TypeScript
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/consistent-type-imports': 'error',
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
-      // React
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'react/self-closing-comp': ['error', { component: true, html: true }],
-
-      // Hooks
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-
-      // General
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-      'prefer-const': 'error',
-
-      // Prettier
-      'prettier/prettier': 'error',
-    },
-    settings: {
-      react: { version: 'detect' },
-      'import/resolver': {
-        typescript: {
-          project: './tsconfig.json',
-        },
-      },
-    },
-    env: {
-      browser: true,
-      node: true,
-      es2022: true,
-    },
-    ignorePatterns: ['.next/', 'node_modules/'],
-  },
-];
+const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')];
+{
+  rules = {
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    'eslint-disable @typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    'eslint-disable @typescript-eslint/await-thenable ': 'off',
+  };
+}
 
 export default eslintConfig;
