770a7987876066538c922bce4a5f9691da259b0d
Foshati <amirrezafoshati@gmail.com>
2025-02-24 21:41:36 +0330

    fix: eslint config and type information and add .vscode file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,16 +14,20 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
     rules: {
       '@typescript-eslint/no-misused-promises': 'off',
       '@typescript-eslint/no-floating-promises': 'off',
-      'eslint-disable @typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
-      'eslint-disable @typescript-eslint/await-thenable ': 'off',
       'import/named': 'off',
       'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'error',
       'import/no-unresolved': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
       'no-empty-function': 'error',
@@ -35,7 +39,6 @@ const eslintConfig = [
       '@next/next/no-img-element': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-misused-promises': 'off',
       '@typescript-eslint/no-var-requires': 'off',
     },
   },
